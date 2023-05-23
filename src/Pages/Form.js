bbbbba5e4564f8d87375c935d754e0d01a13395a
Ugiomoh Daniel <ugiomohd@yahoo.com>
2023-05-23 18:34:46 +0100

    form layout

--- src/Pages/Form.js
+++ src/Pages/Form.js
@@ -1,36 +1,46 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import useSlidesStore from '../store/useSlidesStore';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import useSlidesStore from "../store/useSlidesStore";
 
 function Form() {
-    let navigate=useNavigate()
+  let navigate = useNavigate();
 
-    const updateAIResponse = useSlidesStore((state) => state.updateAIResponse);
-    const processForm=()=>{
-        updateAIResponse({
-            status: true,
-            urlVideo:
-                "https://player.vimeo.com/external/189545487.sd.mp4?s=8cd2af1ec08f7ce121a5a6a09c78c05237943524&profile_id=164&oauth2_token_id=57447761",
-            urlAudio:
-                "https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3",
-        })
-navigate('/editor')
-    }
+  const updateAIResponse = useSlidesStore((state) => state.updateAIResponse);
+  const processForm = () => {
+    updateAIResponse({
+      status: true,
+      urlVideo:
+        "https://player.vimeo.com/external/189545487.sd.mp4?s=8cd2af1ec08f7ce121a5a6a09c78c05237943524&profile_id=164&oauth2_token_id=57447761",
+      urlAudio:
+        "https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3",
+    });
+    navigate("/editor");
+  };
   return (
-    <div style={{display:'flex', alignItems:'center', justifyContent:'center', height:'100vh'}}>
-
-  
-    <div  >
-
-              <form onSubmit={processForm} style={{height:'300px', width: '400px', display: 'flex', flexDirection: 'column', justifyContent:'space-evenly'}}>
-<input placeholder='Video Title' />
- <textarea placeholder='Video Description' ></textarea>
-<button type='submit'>Build</button>
-</form>
-
-    </div>
+    <div
+      className="card-container"
+      style={{
+        height: "100vh",
+      }}
+    >
+      <div className="form-group">
+        <form
+          onSubmit={processForm}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <input placeholder="Video Title" />
+          <textarea placeholder="Video Description"></textarea>
+          <button className="button" type="submit">
+            Build
+          </button>
+        </form>
       </div>
-  )
+    </div>
+  );
 }
 
-export default Form
+export default Form;
